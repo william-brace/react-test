06256d05623175a4bcced2451ebaf5aab060c59e
william-brace <william.brace@hotmail.com>
2020-12-14 23:31:29 -0400

    Played around with building a component following a tutorial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This live reload is too sweet
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/counter.jsx';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counter />,
   document.getElementById('root')
 );
 
